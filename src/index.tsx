a22e5dd9493e380957b54c1d302ad7896904cc17
denisselet <den8rr@gmail.com>
2022-11-12 11:11:56 +0200

    fix: moved provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import setupStore from 'state/store';
 import { App } from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-const store = setupStore();
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+root.render(<App />);

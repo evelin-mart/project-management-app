f6b3d247c6be930f286b5800d0071f4a2994b3f3
evelin-mart <evalouis666@gmail.com>
2022-12-05 15:14:24 +0300

    fix: del unused

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -42,7 +42,6 @@ const developers = {
 export const HomePage = () => {
   const theme = useTheme();
   const smUp = useMediaQuery(theme.breakpoints.up('sm'));
-  const mdUp = useMediaQuery(theme.breakpoints.up('md'));
   const { t } = useTranslation();
 
   return (
@@ -155,7 +154,7 @@ export const HomePage = () => {
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
-        ></iframe>
+        />
       </Stack>
     </Container>
   );
